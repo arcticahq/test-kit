045af2e2f027970c8b7f53801e89ee0092a9a035
David Brookes <david@davidbrookes.io>
2020-03-23 13:01:26 +0000

    initial add

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+/*------------------------------------------------------------------
+[Jest Config]
+
+Project:    Arctica Homepage
+Version:    1.0
+-------------------------------------------------------------------*/
+
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$":
+      "<rootDir>/node_modules/@arctica/test-base/jest-preprocess.js"
+  },
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/node_modules/@arctica/test-base/__mocks__/file-mock.js`
+  },
+  testPathIgnorePatterns: [`node_modules`, `.cache`],
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!(gatsby)/*|@arctica/test-base/*)"
+  ],
+  testRegex: "/.*(__tests__\\/.*)|(.*(test|spec))\\.js?$",
+  collectCoverageFrom: ["src/components/**/*.js"],
+  coverageDirectory: "reports/coverage",
+  globals: {
+    __PATH_PREFIX__: ``
+  },
+  testURL: `http://localhost`,
+  setupFiles: [
+    `<rootDir>/node_modules/@arctica/test-base/loadershim.js`
+  ],
+  snapshotSerializers: ["enzyme-to-json/serializer"]
+}
---
+++ loadershim.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configure } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+
+configure({ adapter: new Adapter() })
+
+// eslint-disable-next-line no-underscore-dangle
+global.___loader = {
+  enqueue: jest.fn()
+}
